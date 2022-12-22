4adb520d862f68566738cf1db9b4df7a0065927f
MERCUROUS <sameershekhar100@gmail.com>
2022-12-23 00:07:57 +0530

    added

--- app/src/main/java/com/example/ibratquizassignment/MainActivity.java
+++ app/src/main/java/com/example/ibratquizassignment/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.ibratquizassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,20 +18,24 @@ public class MainActivity extends AppCompatActivity {
     TextView questionNo,question;
     RadioGroup radioGroup;
     Button submit;
+    ImageView optionImage;
     int points=0;
     RadioButton A,B,C,D;
     int index=0;
     int length=QuestionAnswer.question.length;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         questionNo=findViewById(R.id.count);
+        System.out.println(length);
         A=findViewById(R.id.A);
         B=findViewById(R.id.B);
         C=findViewById(R.id.C);
         D=findViewById(R.id.D);
         question=findViewById(R.id.question);
+        optionImage=findViewById(R.id.imageOption);
         submit=findViewById(R.id.submit);
         radioGroup=findViewById(R.id.questiongrp);
 
@@ -40,23 +48,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     void loadQue(){
-        if(index==length-1){
+        if(index==length){
             createDialog();
             return;
         }
         question.setText(QuestionAnswer.question[index]);
-        String options[]=QuestionAnswer.choices[index];
+        String[] options =QuestionAnswer.choices[index];
+        optionImage.setImageResource(QuestionAnswer.imageOptions[index]);
         A.setText(options[0]);
         B.setText(options[1]);
         C.setText(options[2]);
         D.setText(options[3]);
         int c=1+index;
         questionNo.setText("Question Number:"+c+"");
-        index++;
+
     }
 
     private void createDialog() {
-        Toast.makeText(this, ""+points, Toast.LENGTH_SHORT).show();
+        String passStatus = "";
+        if(points > length*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+        new AlertDialog.Builder(this).setTitle(passStatus)
+                .setMessage("Score is "+ points+" out of "+ length)
+                .setPositiveButton("Restart",((dialogInterface, i) -> restartQuiz()))
+                .setNegativeButton("Exit",((dialogInterface, i) -> {android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);}))
+                .setCancelable(false)
+                .show();
+    }
+
+    private void restartQuiz() {
+       index=0;
+       points=0;
+       loadQue();
     }
 
     void next(){
@@ -69,6 +96,8 @@ public class MainActivity extends AppCompatActivity {
             RadioButton rb=(RadioButton) findViewById(selectedId);
             String radioText=rb.getText().toString();
             if(radioText.equals(QuestionAnswer.correctAnswers[index])) points++;
+            radioGroup.clearCheck();
+            index++;
             loadQue();
         }
 
--- app/src/main/java/com/example/ibratquizassignment/QuestionAnswer.java
+++ app/src/main/java/com/example/ibratquizassignment/QuestionAnswer.java
@@ -2,24 +2,30 @@ package com.example.ibratquizassignment;
 
 public class QuestionAnswer {
 
-    public static String question[] ={
+    public static String[] question ={
             "Which company owns the android?",
-            "Which one is not the programming language?",
-            "Where you are watching this video?",
-            "Which company owns the Apple?"
+            "What is the capital of india?",
+            "Who is the father of android?",
+            "Will I be selected for the interview"
     };
 
-    public static String choices[][] = {
+    public static String[][] choices = {
             {"Google","Apple","Nokia","Samsung"},
-            {"Java","Kotlin","Notepad","Python"},
-            {"Facebook","Whatsapp","Instagram","Youtube"},
-            {"Google","Apple","Nokia","Samsung"}
+            {"Bangalore","Mumbai","Chennai","Delhi"},
+            {"Tim Berners-Lee","Andy Rubin","Steve Jobs","Riyan Parag"},
+            {"Yes","Yes","Yes","Yes"}
     };
 
-    public static String correctAnswers[] = {
+    public static String[] correctAnswers = {
             "Google",
-            "Notepad",
-            "Youtube",
-            "Apple"
+            "Delhi",
+            "Andy Rubin",
+            "Yes"
+    };
+    public static int[] imageOptions={
+            R.drawable.android_logo,
+            R.drawable.india_gate,
+            R.drawable.andy_rubin,
+            R.drawable.happy_face
     };
 }
